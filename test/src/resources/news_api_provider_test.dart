2f06777f18fc3addd004842b89c2ce1459bd8b8d
Dan McAtee <info@danmcatee.com>
2019-01-27 11:53:22 +0100

    Implement new api provider

---
+++ test/src/resources/news_api_provider_test.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:test/test.dart';
+import 'package:http/testing.dart';
+
+import 'package:news/src/resources/news_api_provider.dart';
