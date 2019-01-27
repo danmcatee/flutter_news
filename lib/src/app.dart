ffda816d92898e6806a1342c2f51d38a314e4f77
Dan McAtee <info@danmcatee.com>
2019-01-27 16:49:50 +0100

    Implement StoriesBloc Provider

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'screens/news_list.dart';
+
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      title: 'News',
+      home: NewsList(),
     );
   }
 }
