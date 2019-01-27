8b244a3ae0c91366393ee61a4edd300d0d782e7a
Dan McAtee <info@danmcatee.com>
2019-01-27 17:46:26 +0100

    TODO: fix network request

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
 
+import '../blocs/stories_provider.dart';
+
 class NewsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final bloc = StoriesProvider.of(context);
+    bloc.fetchTopIds();
     return Scaffold(
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: Text('News List'),
+      body: buildList(bloc),
+    );
+  }
+
+  Widget buildList(StoriesBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.topIds,
+      builder: (BuildContext context, AsyncSnapshot<List<int>> snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return ListView.builder(
+          itemCount: snapshot.data.length,
+          itemBuilder: (BuildContext context, int index) {
+            print(snapshot.data);
+            return Text(snapshot.data[index].toString());
+          },
+        );
+      },
     );
   }
 }
